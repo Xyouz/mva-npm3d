3a8fad9f627f121ed57b4b968c8fcadf58d19ac6
Xyouz <marius.dufraisse@gmail.com>
2020-02-12 16:05:24 +0100

    Full numpy, cook not working

--- tp4/MVA_NPM_TP_4.py
+++ tp4/MVA_NPM_TP_4.py
@@ -8,7 +8,7 @@ class Material():
     def f(self, wi, wo, n):
         pass
 
-    def shade(self, normal, light):
+    def shade(self, normal, lights):
         nlig, ncol, _ = normalimage.shape
         render = np.zeros((nlig, ncol, 3))
 
@@ -17,12 +17,15 @@ class Material():
         j = (2*j - ncol)/ncol
 
         wo = np.stack((-j, -i, 1.5 * np.ones_like(i)), axis=-1)
-        wi = light.position - np.stack((-j, -i, np.zeros_like(i)), axis=-1)
+        
+        for light in lights:
+            wi = light.position - np.stack((-j, -i, np.zeros_like(i)), axis=-1)
 
-        f = self.f(wi, wo, normal)
+            f = self.f(wo, wi, normal)
 
-        Li = light.intensity * light.color
-        render = np.clip(f*Li*np.sum(normal*wi,axis=-1,keepdims=True), 0, None)
+            Li = light.intensity * light.color
+            nwi = np.sum(normal*wi,axis=-1,keepdims=True)
+            render += np.clip(f*Li*nwi, 0, None)
         return render 
 
     def __add__(self, other):
@@ -51,38 +54,41 @@ class BlinnPhong(Material):
 
     def f(self, wo, wi, n):
         mid = wo + wi
-        wh = mid / np.linalg.norm(mid)
-        return self.diffuse * np.sum(n*wh,axis=-1,keepdims=True)**self.shine
+        wh = mid / np.linalg.norm(mid, axis=-1, keepdims=True)
+        nwh = np.sum(n*wh, axis=-1, keepdims=True)
+        return self.diffuse * nwh**self.shine
 
 class Cook(Material):
-    def __init__(self, roughness, metal, specular):
-        self.roughness = roughness
-        self.alpha_sq = self.roughness ** 4
-        self.metal = metal
-        self.spec = np.array(specular)
+    def __init__(self, alpha, specular):
+        self.alpha = alpha
+        self.roughness = np.sqrt(self.alpha)
+        self.k = (self.roughness + 1)**2/8
+        self.F0 = np.array(specular)
     
+
     def f(self, wo, wi, n):
         mid = wo + wi
         wh = mid / np.linalg.norm(mid)
 
-        D = self.alpha_sq / (np.pi *(np.dot(n,wh)**2*(self.alpha_sq-1)+1)**2)
+        nwh = np.sum(n*wh, axis=-1, keepdims=True)
+        nwi = np.sum(n*wi, axis=-1, keepdims=True)
+        nwo = np.sum(n*wo, axis=-1, keepdims=True)
+
+        D = self.alpha**2 / (np.pi *(nwh**2*(self.alpha**2-1)+1)**2)
         
         k = (self.roughness + 1)**2 / 8
-        nwi = np.dot(n,wi)
+        
         Gi = nwi/(nwi*(1-k)+k)
-
-        nwo = np.dot(n,wo)
         Go = nwo/(nwo*(1-k)+k)
 
         G = Gi * Go
 
         c1 = -5.55473
         c2 = -6.98316
-        ih = np.dot(wh, wi)
-        F = self.metal + (1 - self.metal)*2**((c1*ih + c2)*ih)
+        oh = np.sum(wh * wo, axis=-1, keepdims=True)
+        F = self.F0 + (1 - self.F0)*2**((c1*oh + c2)*oh)
 
-        return D*F*G/(4 * np.dot(n,wi) * np.dot(n,wo))
-        
+        return D*F*G/(4 * nwi * nwo)
         
 
     
@@ -97,21 +103,18 @@ def shade(normalimage, material, lightsources):
     render = np.zeros((nlig, ncol, 3))
 
     for i, j in np.ndindex(nlig, ncol):
-        if False and np.all(normalimage[i,j,:3] <= 3/255):
-            render[i,j] = [0.,0.,0]
-        else:
-            for ls in lightsources:
-                n = 2 * (normalimage[i,j,:3]-0.5)
-                #n = n/np.linalg.norm(n)
-                x = (2*j - ncol)/ncol
-                y = -(2*i - nlig)/ncol
-                wo = np.array([-x, -y ,1.5])
-                #wo = wo/np.linalg.norm(wo)
-                wi = ls.position - np.array([x,y,0])
-                #wi = wi/np.linalg.norm(wi)
-                f = material.f(wo, wi, n)
-                Li =  ls.intensity * ls.color
-                render[i,j] += np.clip(f * Li * np.dot(n, wi), 0, None)
+        for ls in lightsources:
+            n =  (normalimage[i,j,:3]-0.5)
+            n = n/np.linalg.norm(n)
+            x = (2*j - ncol)/ncol
+            y = -(2*i - nlig)/ncol
+            wo = np.array([-x, -y ,1.5])
+            wo = wo/np.linalg.norm(wo)
+            wi = ls.position - np.array([x,y,0])
+            wi = wi/np.linalg.norm(wi)
+            f = material.f(wo, wi, n)
+            Li =  ls.intensity * ls.color
+            render[i,j] += np.clip(f * Li * np.dot(n, wi), 0, None)
     return render 
 
 if __name__ == "__main__":
@@ -128,23 +131,60 @@ if __name__ == "__main__":
 
     # Define materials and light source
     # lambert = Lambert([0.3, 0.8, 0.5], 3)
-    lambert = Lambert([1.,1.,1.],2)
-    blinn = BlinnPhong([1,1,1.],2)
-    cook = Cook(0.7, 0.25, [0,0,1])
-    material = lambert + blinn# + cook
+    lambert = Lambert([0.70,0.62,0.62],1)
+    lambert2 = Lambert([0.70,0.62,0.62],0.6)
 
-    light_source1 = LightSource([0,1.,1.], [1.,1.,1.], 0.5)
-    light_source2 = LightSource([1.,0.,1.], [1.,0.1,0.3], 0.3)
-    light_source3 = LightSource([-1,0.,1.], [0.1,0.4,1.], 0.3)
+    blinn = BlinnPhong([0.70,0.62,0.62],0.75)
 
-    render = blinn.shade(normalimage[:,:,:3], light_source1) #, light_source2, light_source3])
-    
-    plt.imshow(render)
+    cook = Cook(0.3, [0.70,0.62,0.62])
+    material = blinn
+
+    light_source1 = LightSource([0.,0,1.0], [1.,1.,1.], 0.5)
+    light_source2 = LightSource([1.,0.,1.], [1.,0.1,0.3], 5)
+    light_source3 = LightSource([-1,0.,1.], [0.1,0.4,1.], 5)
+
+    fig, ax = plt.subplots()
+    render = lambert.shade(normalimage[:,:,:3], [light_source1])
+    im = plt.imshow(render)
+    nlig,ncol,_ = render.shape
+
+    from matplotlib.widgets import Slider
+    axx = plt.axes([0.25, 0.1, 0.65, 0.03])
+    axy = plt.axes([0.25, 0.15, 0.65, 0.03])
+    lx = Slider(axx,"x",-1.,1.,0)
+    ly = Slider(axy,"y",-1.,1.,0)
+
+    def onclick(event):
+        x = 5*(event.x-ncol/2)/ncol
+        y = -5*(event.y-nlig/2)/ncol
+        print(x,y)
+        light = LightSource([x,y,1.], [1.,1.,1.], 1)
+        render = lambert.shade(normalimage[:,:,:3],[light])
+        im.set_data(render)
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
+    #lx.on_changed(update)
     plt.show()
-    render = shade(normalimage[:,:,:3],blinn ,[light_source1]) #, light_source2, light_source3])
+    # from  tqdm import tqdm
+    # files = []
+    # for i, x in tqdm(enumerate(np.linspace(-1,1,100))):
+    #     light = LightSource([x,x,1.0], [1.,1.,1.], 0.75)
+    #     render = blinn.shade(normalimage[:,:,:3], [light])#, light_source2, light_source3])
+    #     plt.cla()
+    #     plt.imshow(render)
+    #     fname = "tmp-{}.png".format(i)
+    #     plt.savefig(fname)
+    #     files.append(fname)
     
-    plt.imshow(render)
-    plt.show()
+    # import subprocess
+    # subprocess.call("mencoder 'mf://tmp-*.png' -mf type=png:fps=10 -ovc lavc "
+    #             "-lavcopts vcodec=wmv2 -oac copy -o animation.mpg", shell=True)
 
-    print(render.min(), render.max())
-    plt.imsave(renderfile, render)
+    # for fname in files:
+    #     os.remove(fname)
+
+    # render = shade(normalimage[:,:,:3],cook ,[light_source1])
+    # plt.imshow(render)
+    # plt.show()
+
+    
